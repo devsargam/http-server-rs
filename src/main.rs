f6c6b839ce3aebe4b713341259f67deb79aab03f
Sargam Poudel <sargampoudel100@gmail.com>
2025-09-26 01:24:31 +0545

    impl routes

--- src/main.rs
+++ src/main.rs
@@ -22,19 +22,28 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("./src/hello.html").unwrap();
-    let length = contents.len();
+    match request_line.as_str() {
+        "GET / HTTP/1.1" => {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = fs::read_to_string("./src/hello.html").unwrap();
+            let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+        "GET /sargam HTTP/1.1" => {
+            let status_line = "HTTP/1.1 200 OK";
 
-    println!("{:#?}", http_request);
+            let response = format!("{status_line}\r\nContent-Length: 6\r\n\r\nsargam");
+
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+        _ => {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
 }
